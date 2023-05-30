3d020a03a8a86ad7e273df704af22b5e45891c35
jess-cameron <133481697+jess-cameron@users.noreply.github.com>
2023-05-30 20:36:36 +1000

    first python class

---
+++ Session_1/exercises.py
@@ -0,0 +1,19 @@
+# Q1 Write a program that takes two numbers from the user,and outputs their sum
+# first_number = input("Please enter a number")
+# second_number = input("Please enter another number")
+# print(f"The total is {int(first_number) + int(second_number)}")
+
+# Q2 Write a program that takes two numbers from the user,and outputs the equation representing the multiplication ofthe two numbers
+# first_number = input("Please enter a number")
+# second_number = input("Please enter another number")
+# print(f"The total is {int(first_number) * int(second_number)}")
+
+# Q3 Write a program that takes a distance in kilometers from the user, and output the distance in meters and centimeters.
+# distance_kms = input("Please enter a distance in kms")
+# print(f"{distance_kms}km = {int(distance_kms) * 1000}m")
+# print(f"{distance_kms}km = {int(distance_kms) * 10000}cm")
+
+# Q4 Write a program that takes the user's name and height(in centimeters), and outputs a summary sentence
+# first_name = input("What is your name?")
+# height = input("What is your height in cms?")
+# print(f"{first_name} is {height}cms tall")
